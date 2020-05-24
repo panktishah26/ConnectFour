1cfbe14214ff4cd8b901ee1248e477a1d7bb0b8e
panktishah26 <shah.pankti2609@gmail.com>
2020-05-24 13:02:41 -0700

    Main activity Page

--- app/src/main/java/com/example/connectfour/MainActivity.java
+++ app/src/main/java/com/example/connectfour/MainActivity.java
@@ -5,17 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button computer, friend, profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        computer =(Button)findViewById(R.id.button);
+        friend =(Button)findViewById(R.id.button2);
+        profile =(Button)findViewById(R.id.button3);
+
+        computer.setOnClickListener(new MainActivity.PlayWithComputer());
+        friend.setOnClickListener(new MainActivity.PlayWithFriend());
+        profile.setOnClickListener(new MainActivity.ViewProfile());
     }
 
-    public void play(View view) {
-        Intent  i =new Intent(getApplicationContext(),GameActivity.class);
-        startActivity(i);
+
+    private class PlayWithComputer implements View.OnClickListener {
+        public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this, GameActivity.class));
+        }
+    }
+
+    private class PlayWithFriend implements View.OnClickListener {
+        public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this, GameActivity.class));
+        }
+    }
+
+    private class ViewProfile implements View.OnClickListener {
+        public void onClick(View v) {
+            startActivity(new Intent(MainActivity.this, GameActivity.class));
+        }
     }
 }
